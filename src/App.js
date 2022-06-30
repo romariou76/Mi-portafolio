c2b295f5afd9de6171e1c0281afe99a119209304
romariou76 <illeniumr@gmail.com>
2022-06-30 18:11:14 -0500

    He creado el apartado de About me con sus estilos css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Perfil from './components/Perfil';
-
+import Aboutme from './components/Aboutme';
 
 function App() {
   return (
     <div className="App">
       <div>
       <Navbar/>
-        <Perfil/>
+      <Perfil/>
+      <Aboutme/>
     </div>
     </div>
   );
---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,30 @@
+import '../css/aboutme.css'
+import garrix from '../images/garrix.jpg';
+
+function Aboutme(){
+    return(
+        <div class="about">
+  <div class="caja">
+    <div class="about2">
+      <img class="foto2" src={garrix} alt="perfil"/>
+    </div>
+      <div class="about3">
+        <h1>Sobre mi:</h1>
+        <u>________________</u>
+        <br/>
+        <br/>
+        <p>hola mi nombres es Romario.
+          Soy un desarrollador Web Frontend que actualmente estudia Ing de Software en SENATI. 
+          Siempre he sido una persona positiva, responsable y disciplinada y social. Cuando descubri 
+          el diseño Web en SENATI, me di cuenta que es mi pasion.   </p><br/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit amet sint maxime tempore nostrum distinctio in laudantium illo saepe quos</p>
+        <br/>
+        <a href="#a" class="boton1">Here Me</a>
+        <a href="#a" class="boton2">Download CV</a>
+  </div>
+</div>
+</div>
+    )
+}
+
+export default Aboutme;
