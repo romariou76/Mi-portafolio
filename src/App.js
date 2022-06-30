d540257cd7c1543923dddb6563fb4806f02cf7c5
romariou76 <illeniumr@gmail.com>
2022-06-30 18:21:53 -0500

    He creado un area de mis skills con sus iconos png

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Perfil from './components/Perfil';
 import Aboutme from './components/Aboutme';
+import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Perfil/>
       <Aboutme/>
+      <Skills/>
       <Projects/>
       <Footer/>
     </div>
