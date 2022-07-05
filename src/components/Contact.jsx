e8c50363b6e7fa401bc9c58d57853f5bb5ba6337
romariou76 <illeniumr@gmail.com>
2022-07-05 14:42:42 -0500

    optimize el formulario de contactos con un metodo de envio a gmail por el momento, ademas cree la carpeta svg donde la estan archivos png las cuales usare para un area de follow me.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,26 +7,23 @@ function Contact(){
                 {/* <div className="form-info">
                     <img className='foto' src='#foto' alt='foto'></img>
                 </div> */}
-                    <h1>Contactome</h1>
-				            <h3 className='form-h3'>Escríbeme y me pondre en contacto contigo</h3>
-                    <form action='submit' method='post'>
+                <h1 className='form-titulo'>Contactame</h1>
+                <br></br>
+                <br></br>
+                    <form action='https://formsubmit.co/romario91546@gmail.com' method='post'>
 
-                    <p className='form-p'>
-	    			          <label for="nombre" class="colocar_nombre">Nombre</label>
-	    			          <input type="text" name="introducir_nombre" id="nombre" required="obligatorio" placeholder="Escribe tu nombre"/>
-	    		          </p>
+	    			    <label for="nombre" class="colocar_nombre">Nombre</label>
+	    			    <input type="text" name="nombre" id="nombre" required="obligatorio" placeholder="Escribe tu nombre"/>
                 
-	    		          <p className='form-p'>
-	    			          <label for="email" class="colocar_email">Email</label>
-	    				        <input type="email" name="introducir_email" id="email" required="obligatorio" placeholder="Escribe tu Email"/>
-	    		          </p>
+	    			    <label for="email" class="colocar_email">Email</label>
+	    		        <input type="email" name="correo" id="email" required="obligatorio" placeholder="Escribe tu Email"/>
 
-                    <p className='form-p'>
-					            <label for="mensaje" class="colocar_mensaje">Mensaje</label>                     
-               		    <textarea name="introducir_mensaje" class="texto_mensaje" id="mensaje" required="obligatorio" placeholder="Deja aquí tu comentario..."></textarea> 
-               	    </p>	  								
-			
-				              <button classname="form-button" type="submit" name="enviar_formulario" id="enviar"><p className='button-p'>Enviar</p></button>
+				        <label for="mensaje" class="colocar_mensaje">Mensaje</label>                     
+       		            <textarea name="mensaje" class="texto_mensaje" id="mensaje" required="obligatorio" placeholder="Deja aquí tu comentario..."></textarea>				              
+                        
+                        <button classname="form-button" type="submit" id="enviar"><p className='button-p'>Enviar</p></button>
+                        <input type="hidden" name="_next" value="http://localhost:3000/" ></input>
+                        <input type="hidden" name='_captcha' value="false"></input>
                     </form>
                 </div>
             </div>
