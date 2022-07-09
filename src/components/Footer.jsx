1f11d943d5667546393a1baaf616c30d9ef74df0
romariou76 <illeniumr@gmail.com>
2022-07-08 19:05:16 -0500

    Añadi nuevos iconos en el footer ademas de darles funcionalidad para que direcciones a un determinado link social

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import spotify from '../svg/spotify.png';
 import facebook from '../svg/facebook.png';
 import tiktok from '../svg/tiktok.png';
 import twiter from '../svg/twiter.png';
-import snapchat from '../svg/snapchat.png';
+import whatsapp from '../svg/whatsapp.png';
 
 function Footer(props){
     return(
@@ -26,25 +26,38 @@ function Footer(props){
 <div className='Follow-footer'>
  <div className='Follow-container'>
         <div className='follow_icons'>
+          <a href='https://www.instagram.com/romario_0304/' target='_blank' rel="noopener noreferrer">
           <img class="foto_follow" src={instagram} alt="instagram"/>
+          </a>
         </div>
         <div className='follow_icons'>
+        <a href='https://www.youtube.com/channel/UCA2nHp2r5czagmJswhZYUhQ/videos' target='_blank' rel="noopener noreferrer">
           <img class="foto_follow" src={youtube} alt="youtube"/>
+        </a>
         </div>
         <div className='follow_icons'>
-            <img class="foto_follow" src={spotify} alt="spotify"/>
+          <a href='https://open.spotify.com/user/lixoj5sqhcut5c52pc7he2763?si=UNERXt7mRu69yReuzAmgUg&utm_source=copy-link'>
+          <img class="foto_follow" src={spotify} alt="spotify"/>
+
+          </a>
         </div>
         <div className='follow_icons'>
+          <a href='https://wa.me/51900151546?text=Hola%20me%20gustaria%20conocer%20mas%20de%20ti%20para%20trabajar' target='_blank' rel="noopener noreferrer">
           <img class="foto_follow" src={facebook} alt="facebook"/>
+          </a>
         </div>
         <div className='follow_icons'>
           <img class="foto_follow" src={tiktok} alt="tiktok"/>
         </div>
         <div className='follow_icons'>
+          <a href='https://twitter.com/Charmilion8' target='_blank' rel="noopener noreferrer">
           <img class="foto_follow" src={twiter} alt="twiter"/>
+          </a>
         </div>
         <div className='follow_icons'>
-          <img class="foto_follow" src={snapchat} alt="snapchat"/>
+          <a href='https://wa.me/51900151546?text=Hola%20me%20gustaria%20conocer%20mas%20de%20ti%20para%20trabajar' target='_blank' rel="noopener noreferrer">
+          <img class="foto_follow" src={whatsapp} alt="snapchat"/>
+          </a>
         </div>
         {/* <div className='follow_icons'>
           <img class="foto_follow" src={aple} alt="aple"/>
